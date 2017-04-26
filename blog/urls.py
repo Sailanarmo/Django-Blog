47685e144d70607a0f570a4683965d78f2ac5a6e
Raul Ramirez <rlramirez12@gmail.com>
2017-04-26 03:35:31 +0000

    Weird bug, don't know what to do.

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^blog/view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
-    url(r'^blog/category/(?P<slug>[^\.]+).html', views.view_category, name = 'view_category'),
+    url(r'^blog/category/(?P<slug>[^\.]+).html', views.view_category, name = 'view_blog_category'),
 ]
