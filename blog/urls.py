3e9f57a74f4f97b049f3bbf694c79ec276a9924b
Raul Ramirez <rlramirez12@gmail.com>
2017-04-26 20:29:18 +0000

    Updating

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^blog/view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
-    url(r'^blog/category/(?P<slug>[^\.]+).html', views.view_category, name = 'view_blog_category'),
+    url(r'^view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
+    url(r'^category/(?P<slug>[^\.]+).html', views.view_category, name = 'view_blog_category'),
 ]
